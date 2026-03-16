483f1e1c39eb8f7dcc9276b60a3ecfefdccf9e76
Yuriy Artemyev <anuvyklack@gmail.com>
2026-03-16 04:28:18 +0300

    rename module: corfu -> completion

--- init.example.el
+++ init.example.el
@@ -70,6 +70,7 @@
 
 (require 'helheim-core)
 (require 'helheim-minibuffer) ; Emacs alternative of command pallet
+(require 'helheim-completion)
 
 ;;; Color theme
 
@@ -93,7 +94,6 @@
 
 ;;; Search and completion
 
-(require 'helheim-corfu)    ; Code completion menus
 (require 'helheim-consult)  ; A set of search commands with preview
 (require 'helheim-deadgrep) ; Interface to Ripgrep
 (require 'helheim-embark)   ; Context-aware action menus
