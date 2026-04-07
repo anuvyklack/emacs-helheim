269fb730f536ab30941f9e79563217b958e909ef
Yuriy Artemyev <anuvyklack@gmail.com>
2026-04-07 14:06:37 +0300

    feat: add module for speech-to-text conversion

---
+++ user-lisp/helheim/speech-to-text/helheim-whisper.el
@@ -0,0 +1,22 @@
+;;; helheim-whisper.el -*- lexical-binding: t; no-byte-compile: t -*-
+
+(setup whisper
+  (:install whisper :host github :repo "natrys/whisper.el")
+  (setopt whisper-install-directory user-emacs-directory
+          whisper-language "auto" ;; "en"
+          whisper-model "base"
+          whisper-translate nil
+          whisper-return-cursor 'end ;; 'start
+          whisper-use-threads (ceiling (* 0.7 (num-processors))))
+  (keymap-global-set "C-r" 'whisper-run))
+
+(hel-define-advice whisper--insert-text (:around (orig-fun &rest args))
+  (let ((deactivate-mark nil))
+    (apply orig-fun args)
+    (hel-normal-state)
+    (hel-set-region whisper--marker (point)
+                    (if (eq whisper-return-cursor 'start) -1))))
+
+;;; .
+(provide 'helheim-whisper)
+;;; helheim-whisper.el ends here
