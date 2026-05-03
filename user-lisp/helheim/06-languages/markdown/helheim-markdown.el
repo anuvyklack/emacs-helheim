f349234872c62af308ac82b1fde2c6084e92ae88
Yuriy Artemyev <anuvyklack@gmail.com>
2026-05-03 22:43:24 +0300

    markdown: fix closing parentheses

--- user-lisp/helheim/06-languages/markdown/helheim-markdown.el
+++ user-lisp/helheim/06-languages/markdown/helheim-markdown.el
@@ -52,7 +52,7 @@
                    "<style> body { box-sizing: border-box; max-width: 740px; width: 100%; margin: 40px auto; padding: 0 10px; } </style>"
                    "<script id='MathJax-script' async src='https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js'></script>"
                    "<script src='https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js'></script>"
-                  "<script>document.addEventListener('DOMContentLoaded', () => { document.body.classList.add('markdown-body'); document.querySelectorAll('pre[lang] > code').forEach((code) => { code.classList.add(code.parentElement.lang); }); document.querySelectorAll('pre > code').forEach((code) => { hljs.highlightBlock(code); }); });</script>"))))
+                  "<script>document.addEventListener('DOMContentLoaded', () => { document.body.classList.add('markdown-body'); document.querySelectorAll('pre[lang] > code').forEach((code) => { code.classList.add(code.parentElement.lang); }); document.querySelectorAll('pre > code').forEach((code) => { hljs.highlightBlock(code); }); });</script>"))))))
 
 (defun helheim-markdown-mode-h ()
   (setq tab-width 2
