c7ef7b1b8402bd5855ba55061939607b8657e6ae
Yuriy Artemyev <anuvyklack@gmail.com>
2026-05-30 18:05:37 +0300

    feat(ghostel): add keys and documentation

--- user-lisp/helheim/07-terminal/ghostel/helheim-ghostel.el
+++ user-lisp/helheim/07-terminal/ghostel/helheim-ghostel.el
@@ -7,7 +7,22 @@
   (:hook ghostel-mode-hook (lambda ()
                              ;; Do not enable current line highlighting in
                              ;; ghostel emacs and copy modes.
-                             (setq ghostel--saved-hl-line-mode nil))))
+                             (setq ghostel--saved-hl-line-mode nil)))
+  (:global-bind
+    "C-c o t" 'ghostel
+    ;; ghostel-next
+    ;; ghostel-previous
+    ;; ghostel-list-buffers
+    ;; ghostel-project-list-buffers
+    ))
+
+(setup project
+  (:after-load
+    (add-to-list 'project-kill-buffer-conditions '(derived-mode . ghostel-mode))
+    (keymap-set project-prefix-map "t" 'ghostel-project)
+    (when-let* ((idx (-elem-index '(project-eshell "Eshell") project-switch-commands)))
+      (cl-callf ->> project-switch-commands
+        (-insert-at (1+ idx) '(ghostel-project "Terminal"))))))
 
 (setup hel-ghostel
   (:install hel-ghostel :host github :repo "anuvyklack/hel-ghostel")
